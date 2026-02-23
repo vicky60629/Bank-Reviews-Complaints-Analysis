48c25dbd5a98532369139c4b23d2e4701b1835b6
Vicky Gupta <44772160+vicky60629@users.noreply.github.com>
2026-02-23 12:32:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask,render_template,url_for,request
-import pandas as pd
 import joblib
+import os
 
 # load the model from disk
 model = joblib.load('nlp_model.pkl')
 cv = joblib.load('transform.pkl')
-tf = joblib.load('tfidf_transform.pkl')
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,15 +14,17 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
 
+
+
 	if request.method == 'POST':
 		message = request.form['message']
 		data = [message]
 		vect = cv.transform(data)
-		tfidf = tf.transform(vect)
-		my_prediction = model.predict(tfidf)[0]
+		my_prediction = model.predict(vect)[0]
 	return render_template('result.html',prediction = my_prediction)
 
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
